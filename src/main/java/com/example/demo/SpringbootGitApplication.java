b5996c7e25cdb5457aed7299cd8fc78991563930
luoyuan <909796304@qq.com>
2018-10-17 11:10:08 +0800

    第一天提交工作

--- src/main/java/com/example/demo/SpringbootGitApplication.java
+++ src/main/java/com/example/demo/SpringbootGitApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootGitApplication {
 
 	public static void main(String[] args) {
-		System.out.println("第一天提交工作");
+		System.out.println("第一天提交工a作");
 		SpringApplication.run(SpringbootGitApplication.class, args);
 	}
 }
